39f2699b90174dcfb0bad2525f35375915cdd0f7
Vladimír Vondruš <mosra@centrum.cz>
2019-06-11 15:05:04 +0200

    python: more yelling at the buildsystem.

--- package/ci/setup-pybind11.sh
+++ package/ci/setup-pybind11.sh
@@ -7,8 +7,10 @@ cd pybind11-2.2.4
 # Patch the CMake macro to add include paths as SYSTEM because otherwise GCC
 # 4.8 gets very loud. This is already done in
 # https://github.com/pybind/pybind11/pull/1416 but it's opt-in and not part of
-# any release yet. I don't want to force users to use pybind11 git, so apply
-# a patch manually.
+# any release yet. For Git releases this is handled inside
+# src/python/CMakeLists.txt (detecting version and applying SYSTEM), but I want
+# to ensure compatibility with 2.2.4 *and* have useful CI output, so I am
+# patching it here as well.
 patch -p1 < $TRAVIS_BUILD_DIR/package/ci/pybind11-system-includes.patch
 
 mkdir -p build && cd build
