e4aa19884a9e83b799389b04eca76e8738941bcb
Vladimír Vondruš <mosra@centrum.cz>
2019-07-15 12:35:19 +0200

    package/ci: use pybind11 2.3.0.

--- package/ci/setup-pybind11.sh
+++ package/ci/setup-pybind11.sh
@@ -1,17 +1,8 @@
 set -e
 
-wget --no-clobber https://github.com/pybind/pybind11/archive/v2.2.4.tar.gz && tar -xzf v2.2.4.tar.gz
+wget --no-clobber https://github.com/pybind/pybind11/archive/v2.3.0.tar.gz && tar -xzf v2.3.0.tar.gz
 
-cd pybind11-2.2.4
-
-# Patch the CMake macro to add include paths as SYSTEM because otherwise GCC
-# 4.8 gets very loud. This is already done in
-# https://github.com/pybind/pybind11/pull/1416 but it's opt-in and not part of
-# any release yet. For Git releases this is handled inside
-# src/python/CMakeLists.txt (detecting version and applying SYSTEM), but I want
-# to ensure compatibility with 2.2.4 *and* have useful CI output, so I am
-# patching it here as well.
-patch -p1 < $TRAVIS_BUILD_DIR/package/ci/pybind11-system-includes.patch
+cd pybind11-2.3.0
 
 mkdir -p build && cd build
 cmake .. \
