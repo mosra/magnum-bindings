b83e12a8fd5b01d252ef9e48e9705bd1ff09f99c
Vladimír Vondruš <mosra@centrum.cz>
2020-10-22 19:11:56 +0200

    package/homebrew: update to what's actually latest.

--- package/homebrew/magnum-bindings.rb
+++ package/homebrew/magnum-bindings.rb
@@ -1,9 +1,9 @@
 class MagnumBindings < Formula
   desc "`Bindings for the Magnum C++11/C++14 graphics engine"
   homepage "https://magnum.graphics"
-  url "https://github.com/mosra/magnum-bindings/archive/v2019.10.tar.gz"
-  # wget https://github.com/mosra/magnum-bindings/archive/v2019.10.tar.gz -O - | sha256sum
-  # sha256 "7e0038149fd37237efaf8dff52cf7e4ee8629e630c84a4f76bcd0eb166898e8c"
+  url "https://github.com/mosra/magnum-bindings/archive/v2020.06.tar.gz"
+  # wget https://github.com/mosra/magnum-bindings/archive/v2020.06.tar.gz -O - | sha256sum
+  sha256 "959c703e6409ba0c2cd6c0da3a2b6190f6fac837ff69f64cbdc372e11359e7d8"
   head "git://github.com/mosra/magnum-bindings.git"
 
   depends_on "cmake"
@@ -14,7 +14,11 @@ class MagnumBindings < Formula
   def install
     system "mkdir build"
     cd "build" do
-      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DWITH_PYTHON=ON", ".."
+      system "cmake",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+        "-DWITH_PYTHON=ON",
+        ".."
       system "cmake", "--build", "."
       system "cmake", "--build", ".", "--target", "install"
       cd "src/python" do
