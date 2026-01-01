c1a0c534caba5f48963a86f9b83fb30832139aac
Vladimír Vondruš <mosra@centrum.cz>
2026-01-02 00:29:44 +0100

    package/ci: a comment copypasted from magnum-plugins, what?!

--- package/ci/appveyor-desktop.bat
+++ package/ci/appveyor-desktop.bat
@@ -104,8 +104,7 @@ cmake --build . || exit /b
 cmake --build . --target install || exit /b
 cd .. && cd ..
 
-rem Build. BUILD_GL_TESTS is enabled just to be sure, it should not be needed
-rem by any plugin.
+rem Build
 mkdir build && cd build || exit /b
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Release ^
